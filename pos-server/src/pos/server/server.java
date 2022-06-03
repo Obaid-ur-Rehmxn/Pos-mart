d46f4b01ac640131532154fae215eb686fa16400
JamalNasir2112 <102358434+JamalNasir2112@users.noreply.github.com>
2022-06-03 22:45:43 +0500

    Login completed

--- pos-server/src/pos/server/server.java
+++ pos-server/src/pos/server/server.java
@@ -11,10 +11,19 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import static java.util.Collections.list;
+import java.util.List;
 import static javax.swing.UIManager.getString;
 
 /**
@@ -46,7 +55,7 @@ public void serverside(){
                      status=login_validation(username,userpass);
                 }
                 outToClient.writeBytes(status +'\n');
-//              
+              
             }
         }
             catch(Exception e){
@@ -56,34 +65,66 @@ public void serverside(){
 } 
 public String login_validation(String username,String userpass) throws SQLException {
     String status="invalid";
-    System.out.println(username);
-    System.out.println(userpass);
     Connection con = DriverManager.getConnection(ConnectionClass.conString);
     PreparedStatement statement = con.prepareStatement("SELECT * FROM tbl_users");
     ResultSet rs = statement.executeQuery();
      while (rs.next())
              {      
-//                String name = rs.getString("RwName");         
-//                 if("obaid"==username  && "obaid"==userpass){
                 if((rs.getString(2).equals(username)) && (rs.getString(3).equals(userpass))){
                      status="valid";
                 break;
                 }
-
-                 }
-             //end while
+                 }    
              con.close();
              return status;
 }
+
+public void getDataFromUsers() throws SQLException, IOException{
+    String data = " ";
+    String path="C:\\Users\\Jamal\\Desktop\\nabeel.txt";
+     Connection con = DriverManager.getConnection(ConnectionClass.conString);
+    PreparedStatement statement = con.prepareStatement("SELECT * FROM tbl_users");
+    ResultSet rs = statement.executeQuery();
+     while (rs.next()){
+         String b=rs.getString(2);
+         String c=rs.getString(3);
+         data=b.concat(","+c);
+         writeTextToFile(data, path);
+}
+      con.close();
+      sendFile(path);   
+}
+
+public void writeTextToFile(String data,String path) throws IOException{
+     FileWriter file = new FileWriter(path,true);
+         BufferedWriter myWriter=new BufferedWriter(file);
+         myWriter.write(data);
+         myWriter.newLine();
+         myWriter.close();
+         file.close();
+}
+
+public void sendFile(String path) throws IOException{
+    path="C:\\Users\\Jamal\\Desktop\\nabeel.txt";
+    ServerSocket s=new ServerSocket(4333);
+        while(true){
+        Socket sr=s.accept();
+        FileInputStream fr=new FileInputStream(path);
+        byte b[]=new byte[2002];
+        fr.read(b,0,b.length);
+        OutputStream os=sr.getOutputStream();
+        os.write(b, 0, b.length);
+        }
+}
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, IOException {
         server s=new server();
-        s.serverside();
+        s.getDataFromUsers();
         
 }
-
-    }
+}
     
 
